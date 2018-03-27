a64f373d2d30a1b4f9f562e6ac07897df9374aad
rajapattanayak <pattanayak.raja@gmail.com>
2018-03-27 14:35:49 +0530

    Send msg to all client

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,13 @@
 var socket = io();
 
-socket.on("connect", function() {
+socket.on("connect", function () {
   console.log("connected to server");
-
-  socket.emit("createMessage", {
-    from: "raja@gmail.com",
-    text: "How are you guys"
-  });
 });
 
-socket.on("disconnect", function() {
+socket.on("disconnect", function () {
   console.log("User was disconnected");
 });
 
-socket.on("newMessage", function(msg) {
-  console.log(msg);
+socket.on("newMessage", function (message) {
+  console.log(message);
 });
--- server/server.js
+++ server/server.js
@@ -15,10 +15,11 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
   
-  socket.on('createMessage', (newMsg) => {
-    console.log('createMessage', newMsg)
-    newMsg.createdAt = new Date().toString();
-    socket.emit('newMessage', newMsg)
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+
+    message.createdAt = new Date().getTime();
+    io.emit('newMessage', message)
   });
 
   socket.on('disconnect', () => {
